f895c6c3c96301dc9bb4db7ebc7871e4b6a1a817
suraj maru <suraj123maru@gmail.com>
2025-05-31 04:26:33 +0530

    functions,objects,loops and etc

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -246,8 +246,255 @@ let totaltip;
     }
 }
 
-calcTip();
 const tips = [calcTip(v1[0]),calcTip(v1[1]),calcTip(v1[2])];
 
+//
+//objects.
+/*
+const surajj = {
+    firsttName: "suraj",
+    lasttName: "maru",
+    aaage: 2025-2005,
+    jobb: "student",
+    friendss: ["jay","bob","tom"]
+};
+//dot notation and bracket notations.
+console.log(surajj);
+console.log(surajj.lasttName);//way to access object values.
+console.log(surajj["firsttName"]); //way to access object values.
+//
+
+const nameKey = "Name";
+console.log(surajj["firstt"+ nameKey]); //ways
+console.log(surajj["lastt"+ nameKey]); //ways
+//
+// console.log(suraj."lastt"+ nameKey); //not possible here.
+//
+
+const p = prompt("what do you wanna know about suraj? choose between firsttName, lasttName, aaage, jobb, friendss");
+if(surajj[p]){
+    console.log(surajj[p]); // this is a great example for using dot or bracket notation.
+    // console.log(surajj.p);// this wont work for us here.
+    
+}else{
+    console.log("choose one from the provided one!")
+}
+//
+surajj.loaction = "india";
+surajj["twitter"] = "@surajmaru";
+console.log(surajj);
+// here we can make any changes in our created object.
+// and is shown by using 2 diffrent methods.
+//
+
+console.log(`${surajj.firsttName} has ${surajj.friendss.length} friends, and his best friend is called ${surajj.friendss[2]}`);
+//
+*/
+
+const surajj = {
+    firsttName: "suraj",
+    lasttName: "maru",
+    birthhYear: 2005,
+    jobb: "student",
+    friendss: ["jay","bob","tom"],
+    hassDriverLicense: true,
+
+    // calccAge: function (birthhYear){ // this is an expression.
+    //     return 2025-birthhYear; // function can be used inside a object using a key value like here "calccAge",
+    // } // Any function that is attached to an object is called as a method.
+
+    //  calccAge: function (birthhYear){ // this is an function declaration so it wont work here..
+    //     return 2025-birthhYear; 
+    // }
+
+    // calccAge: function (){
+    //     return 2025-this.birthhYear; //Here "this.birthhYear" means the object which is our surajj and then the birthhYear keyword init.
+    //     // so the value inside it is accesed.
+    //     // we can also write "surajj.birthhYear" instead of "this.birthhYear" but it violates the "dont repeat you code rule"
+    //     // and we change our object name from surajj to something else then the code wont work!. so this. is used.
+    // }
+
+    calccAge: function (){
+        this.aaage = 2025-this.birthhYear; 
+        return this.aaage;
+    },
+
+    getSummary: function(){
+        return `${surajj.firsttName} is a ${surajj.aaage}-year old ${surajj.jobb}, and he has ${ surajj.hassDriverLicense? "a":"no" } driver's license`;
+    },
+
+};
+// console.log(surajj.calccAge(surajj.birthhYear));
+// console.log(surajj["calccAge"](surajj.birthhYear)); // here is the o/p for the function expression above in an object.
+console.log(surajj.calccAge()); // first we have to call the function here. Without it, the aaage key is not added in the object. so we have to call/run the function and then use that keyword 
+console.log(surajj.aaage);
+console.log(surajj.aaage);
+console.log(surajj.aaage);
+//
+
+console.log(surajj.getSummary());
+//
+// console.log(typeof yearss)
+
+// challenge
+let mark = {
+
+    fullName: "Mark Miller",
+    mass: 78,
+    height: 1.69,
+    calcBMI: function(){
+         this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    },
+   
+};
+let john = {
+
+    fullName: "John Smith",
+    mass: 92,
+    height: 1.95,
+     calcBMI: function(){
+         this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+
+};
+
+mark.calcBMI();
+john.calcBMI();
+if(mark.bmi>john.bmi){
+console.log(`Mark Miller's BMI ${mark.bmi} is higher than John Smith's ${john.bmi}!`);
+} else {
+    console.log(`John Smith's BMI ${john.bmi} is higher than Mark Miller's ${mark.bmi}!`)
+}
+//
+
+//loops
+// console.log("lifting weights rep 1");
+// console.log("lifting weights rep 2");
+// console.log("lifting weights rep 3");
+// console.log("lifting weights rep 4");
+// console.log("lifting weights rep 5");
+// console.log("lifting weights rep 6");
+// console.log("lifting weights rep 7");
+// console.log("lifting weights rep 8");
+// console.log("lifting weights rep 9");
+// console.log("lifting weights rep 10");
+
+for(let i=1; i<=10; i++){
+    console.log(`lifting weights rep ${i}`)
+}// alternative for above code.
+//
+
+//looping arrays,breaking and continuing.
+const surajArray = [
+    "suraj","maru",2025-2005,"student",["bob","tom","jacob"], true
+];
+const types =[];
+
+for(let i=0; i<surajArray.length; i++){
+    console.log(surajArray[i],typeof surajArray[i] ); // reading from the array.
+
+    //types[i] = typeof surajArray[i]; // filling an array
+    types.push(typeof surajArray[i]); // other method.
+}
+
+console.log(types);
+//
+
+const yearr = [2000,1990,1999,2005,2010];
+const ages = [];
+
+for(let i=0; i<yearr.length;i++){
+
+    ages.push(2025 - yearr[i]);
+}
+console.log(ages);
+//
+
+//continue and break statement.
+
+console.log("---only string---")
+for(let i=0; i<surajArray.length; i++){
+    if(typeof surajArray[i] !== "string")continue // continue statement example.
+    console.log(i,surajArray[i],typeof surajArray[i] ); 
+}
+console.log("---only numbers---")
+for(let i=0; i<surajArray.length; i++){
+
+    if(typeof surajArray[i] !== "number")continue // continue statement example.
+    console.log(i,surajArray[i],typeof surajArray[i] ); 
+}
+//
+
+console.log("---break with number---");
+for(let i=0; i<surajArray.length; i++){
+
+    if(typeof surajArray[i] === "number") break // break statement example.
+    console.log(i,surajArray[i],typeof surajArray[i] ); 
+}
+//
+
+// looping backwards and loops in loops.
+const surajArrayy = [
+    "suraj",
+    "maru",
+    2025-2005,
+    "student",
+    ["bob","tom","jacob"]
+];
+
+for(let i = surajArrayy.length-1; i>=0 ; i--){ // printing a array in backwards. 
+    console.log(i,surajArrayy[i]);
+}
+//
+
+//loops in loops.
+for(let exercise = 1; exercise<=3; exercise++){
+    console.log(`-----starting exercise ${exercise}`)
+
+    for(let rep = 1; rep<=5; rep++){
+        console.log(`exercise ${exercise} lifting weights reps ${rep}`);
+    }
+}
+//
+
+//while loop.
+// for(let rep = 1; rep<=10; rep++){
+//     console.log(`reps ${rep}`)
+// }
+
+let rep = 1;
+while(rep<=10){
+    console.log(`while loop reps ${rep}`);
+    rep++;
+}
+console.log("------");
+let dice = Math.trunc(Math.random()*6) + 1;
+while(dice!==6){
+    console.log(`you rolled a ${dice}`);
+    dice = Math.trunc(Math.random()*6) + 1;
+
+if(dice===6){
+    console.log("loop ended")
+}
+}
+//
+//challenge.
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const ttips = [];
+const ttotals = [];
+
+ const calcTippp = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+for(let i = 0; i<bills.length; i++){
+   
+let w = calcTippp(bills[i]);
+ ttips.push(w);
+ttotals.push(w + bills[i]);
+}
+console.log(bills, ttips, ttotals);
 //
 
