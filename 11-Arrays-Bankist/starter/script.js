9824906a3eb26239e8e86cd4466ea2210ed8fc37
surajmaru <jakejonathan69@gmail.com>
2025-09-30 18:57:15 +0530

    completed array methods

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -469,6 +469,90 @@ const avgHeavy = Math.max(...avgHeavy3); // Math.max(avhHeavy3) will now work ca
 console.log(avgHeavy);
 
 console.log("---Challenge 4 complete---");
+
+console.log("---Challenge 5---");
+
+// Julia and Kate are still studying dogs. This time they are want to figure out if the dogs in their are eating too much or too little food.
+
+// - Formula for calculating recommended food portion: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+// - Eating too much means the dog's current food portion is larger than the recommended portion, and eating too little is the opposite.
+// - Eating an okay amount means the dog's current food portion is within a range 10% above and below the recommended portion (see hint).
+
+// YOUR TASKS:
+// 1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion (recFood) and add it to the object as a new property. Do NOT create a new array, simply loop over the array (We never did this before, so think about how you can do this without creating a new array).
+// 2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple users, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+// 3. Create an array containing all owners of dogs who eat too much (ownersTooMuch) and an array with all owners of dogs who eat too little (ownersTooLittle).
+// 4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+// 5. Log to the console whether there is ANY dog eating EXACTLY the amount of food that is recommended (just true or false)
+// 6. Log to the console whether ALL of the dogs are eating an OKAY amount of food (just true or false)
+// 7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+// 8. Group the dogs into the following 3 groups: 'exact', 'too-much' and 'too-little', based on whether they are eating too much, too little or the exact amount of food, based on the recommended food portion.
+// 9. Group the dogs by the number of owners they have
+// 10. Sort the dogs array by recommended food portion in an ascending order. Make sure to NOT mutate the original array!
+
+// HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
+// HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). Basically, the current portion should be between 90% and 110% of the recommended portion.
+
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John', 'Leo'] },
+  { weight: 18, curFood: 244, owners: ['Joe'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+//1
+dogs.forEach(dogs => (dogs.recFood = Math.floor(dogs.weight ** 0.75 * 28)));
+console.log(dogs);
+//2
+const dogSarah = dogs.find(dog => dog.owners.includes('Sarah')); 
+console.log(dogSarah); 
+//3
+console.log(`sarah's dog eats too ${dogSarah.curFood>dogSarah.recFood? "much":"less"}`);
+//4
+const ownersTooMuch = dogs
+  .filter(dog => dog.curFood > dog.recFood)
+  .flatMap(dog => dog.owners);
+const ownersTooLittle = dogs
+  .filter(dog => dog.curFood < dog.recFood)
+  .flatMap(dog => dog.owners);
+
+  console.log(ownersTooMuch);
+  console.log(ownersTooLittle);
+//5
+  const toMuch = ownersTooMuch.join(" and ");
+  const toLittle = ownersTooLittle.join(" and ");
+  console.log(`${toMuch}'s dogs eat too much! and ${toLittle}'s dogs eat too little!`);
+
+  console.log(dogs.some(w=>w.curFood === w.recFood));
+//6
+  const checkEatingOkay = dog =>
+  dog.curFood < dog.recFood * 1.1 && dog.curFood > dog.recFood * 0.9;
+  console.log(dogs.every(checkEatingOkay));
+//7
+  const dogsEatingOkay = dogs.filter(checkEatingOkay);
+  console.log(dogsEatingOkay);
+//8
+  let grp = Object.groupBy(dogs,dog=>{
+    if (dog.curFood > dog.recFood) {
+    return 'too-much';
+  } else if (dog.curFood < dog.recFood) {
+    return 'too-little';
+  } else {
+    return 'exact';
+  }
+  })
+  console.log(grp);
+//9
+  let grpAge = Object.groupBy(dogs,dog=>`${dog.owners.length}-owners`);
+  console.log(grpAge);
+//10
+  const dd = dogs.toSorted((a, b) => a.recFood - b.recFood);
+  console.log(dd);
+
+console.log("---Challenge 5 complete---");
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // Concepts
@@ -1005,3 +1089,76 @@ labelBalance.addEventListener("click",function(){
   movementsUI2.map(ele => Number(ele.textContent.replace("€","")));
   // Like this, so the "Array.from" is the best way to go.
 });
+
+//Non-Destructive Alternatives: toReversed, toSorted, toSpliced, with
+console.log("---Non-Destructive Alternatives: toReversed, toSorted, toSpliced, with---");
+
+// reverse, splice, sort mutate the original array.
+// so here these methods dont mutate the array instead they return the original array.
+
+//toReverse() method.
+console.log(movements3);
+// const reversed = movements3.slice().reverse(); // previously we used to use this type of chaining to not change the original array.
+const reversed = movements3.toReversed(); // but here is the direct alternative.
+console.log(reversed);
+console.log(movements3);
+
+console.log(movements3);
+
+//toSorted() method, toSpliced() method.
+// console.log(movements3.splice(1,6));
+console.log(movements3.slice().splice(1,6));
+console.log(movements3.toSpliced(1,5));
+console.log(movements3);
+
+// console.log(movements3.sort());
+console.log(movements3.slice().sort());
+console.log(movements3.toSorted());
+console.log(movements3);
+
+// with() method.
+// movements3[1] = 2000;
+const newMovement = movements3.with(1,2000);
+console.log(newMovement);
+console.log(movements3);
+
+
+//
+let aa = 10;
+console.log(++aa);
+
+const depo = accounts.flatMap(acc=> acc.movements)
+.reduce((acc,curr)=> (curr>= 1000? ++acc: acc),0)
+console.log(depo);
+
+//
+const {deposits2,withdrawals2} =  accounts
+.flatMap(acc=> acc.movements)
+.reduce((acc,curr)=> {
+  // curr > 0 ? (acc.deposits2 += curr) : (acc.withdrawals2 += curr);
+  acc[curr > 0 ? "deposits2" : "withdrawals2"] += curr;
+  return acc;
+},{deposits2:0,withdrawals2:0});
+console.log(deposits2,withdrawals2);
+
+//
+// this is a nice title -> This Is a Nice Title
+const convertTitleCase = function (title) {
+  const capitalize = str => str[0].toUpperCase() + str.slice(1);
+
+  const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
+
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word => (exceptions.includes(word) ? word : capitalize(word)))
+    .join(' ');
+
+  return capitalize(titleCase);
+};
+
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a LONG title but not too long'));
+console.log(convertTitleCase('and here is another title with an EXAMPLE'));
+
+//
