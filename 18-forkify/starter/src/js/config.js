8ac9c50dfc10c293c0f28a08fa9305ed4e39d255
surajmaru <jakejonathan69@gmail.com>
2025-11-25 03:55:39 +0530

    implementing mvc architecture and organizing the code

---
+++ 18-forkify/starter/src/js/config.js
@@ -0,0 +1,5 @@
+// Here will be all the constants and those variables who will be reused in our project.
+
+export const API_URL = "https://forkify-api.jonas.io/api/v2/recipes";
+
+export const TIMEOUT_SECONDS = 15;
---
+++ 18-forkify/starter/src/js/model.js
@@ -0,0 +1,30 @@
+import { API_URL } from "./config.js";
+import { getJSON } from "./helpers.js";
+
+export const state = {
+    recipe: {},
+};
+
+export const loadRecipe = async function(id){
+    try{
+    const data = await getJSON(`${API_URL}/${id}`);
+    
+    const {recipe} = data.data; // Creating a new object from the apis object for our better understanding.
+    state.recipe = {
+        id: recipe.id,
+        title: recipe.title,
+        publisher: recipe.publisher,
+        sourceUrl: recipe.source_url,
+        image: recipe.image_url,
+        servings: recipe.servings,
+        cookingTime: recipe.cooking_time,
+        ingredients: recipe.ingredients
+        
+    };
+    // console.log(data.data.recipe);
+    console.log(state.recipe);
+    } catch(err){
+        // Temp error handling
+        console.error(`${err}💥💥💥`);
+    }
+}
